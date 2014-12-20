e80f3f25db595364e1a203c310509bface9aee91
kenith <kenith.777@gmail.com>
2014-12-20 13:13:56 +0530

    refactored the emailer

--- junction/custom_utils/emailer.py
+++ junction/custom_utils/emailer.py
@@ -2,15 +2,15 @@ import sendgrid
 from junction.settings import SENDGRID_FROM_EMAIL, SENDGRID_EMAIL_USERNAME,SENDGRID_EMAIL_PASSWORD
 
 class EmailEngine():
-   
     '''Send the email Body,Subject,To_Address as list, CC_Emails as list IN A DICTIONARY. Optionally 
         you can even send the from_email '''
+  
     def send_email(self, content):    
         
         to_list = content['to_email']
-        try:
-            FROM_ADDRESS = content['from_email']
-        except:
+        
+        FROM_ADDRESS = content.get('from_email', default=None)
+        if FROM_ADDRESS == None:
             FROM_ADDRESS = SENDGRID_FROM_EMAIL
         
         sg = sendgrid.SendGridClient(SENDGRID_EMAIL_USERNAME, SENDGRID_EMAIL_PASSWORD, raise_errors=True)
@@ -20,11 +20,10 @@ class EmailEngine():
         message.set_subject(content['subject'])
         message.set_html(content['body'])
         message.set_from(FROM_ADDRESS)
-        try:
-            cc_list = content['cc_email']
+        
+        cc_list = content.get('cc_email', default=None)
+        if cc_list != None:
             message.add_bcc(cc_list)
-        except:
-            pass
         
         status, msg = sg.send(message)
       
