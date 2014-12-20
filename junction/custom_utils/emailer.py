28adeb85d8f9b0135b70ae0d22a03da62aae1d29
kenith <kenith.777@gmail.com>
2014-12-20 16:27:52 +0530

    refactored the emailer again

--- junction/custom_utils/emailer.py
+++ junction/custom_utils/emailer.py
@@ -3,33 +3,26 @@ from junction.settings import SENDGRID_FROM_EMAIL, SENDGRID_EMAIL_USERNAME,SENDG
 
 class EmailEngine():
   
-    def send_email(self, content):    
-        '''It takes email content, subject, to address, cc-list in a dictionary. Optionally 
-        it t from-email address. Format to send the dictionary is as follows
-        content = {}
-        content['body'] = email body [text or html]
-        content['subject'] = subject
-        content['to_email'] = ['ab@c.com'] or ['ab@c.com','dc@m.com']
-        content['cc_email'] = ['ab@c.com'] or ['ab@c.com','dc@m.com']
-        content['from_email'] = "from_email@zz.com"  optional, since this would have been configured in 
-                                                     settings file
+    def send_email(self,to_list,body,subject=None,cc_list=None,from_address=None):    
+        '''It takes email body, to address and Optionally it takes cc-list, subject 
+        and from-email address. Format to send the dictionary is as follows
+        email body  can be text or html
+        to_email = ['ab@c.com'] or ['ab@c.com','dc@m.com']
+        cc_email = ['ab@c.com'] or ['ab@c.com','dc@m.com']
+        from_email = "from_email@zz.com"  optional, since this would have been configured in settings file
+                                                     
         '''
         
-        to_list = content['to_email']
-        subject = content['subject']
-        body = content['body']
-        cc_list = content.get('cc_email', default=None)
-        from_address = content.get('from_email', default=None)
-        from_address = SENDGRID_FROM_EMAIL or from_address 
-            
-        
+        subject = subject or ""
+        verified_from_address = from_address or SENDGRID_FROM_EMAIL[0]
+       
         sg_handler = sendgrid.SendGridClient(SENDGRID_EMAIL_USERNAME, SENDGRID_EMAIL_PASSWORD, raise_errors=True)
         message = sendgrid.Mail()
         
         message.add_to(to_list)
         message.set_subject(subject)
         message.set_html(body)
-        message.set_from(from_address)
+        message.set_from(verified_from_address)
         if cc_list is not None:
             message.add_bcc(cc_list)
         
