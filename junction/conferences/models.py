bc6b0920e77cf2ca6cc71375aec46ea02e3bbdde
kenith <kenith.777@gmail.com>
2014-12-27 10:09:27 +0530

    email sent when ever conference data changed

--- junction/conferences/models.py
+++ junction/conferences/models.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django_extensions.db.fields import AutoSlugField
+from django.template.loader import render_to_string
+from django.template.context import RequestContext, Context
 
 from custom_utils.constants import CONFERENCE_STATUS_LIST
 from custom_utils.models import AuditModel
+from custom_utils.emailer import EmailEngine
 
+def _get_conference_moderator_emails(conference):
+    return [(cm.moderator.email)
+            for cm in ConferenceModerator.objects.filter(conference=conference)]
 
 class Conference(AuditModel):
     """ Conference/Event master """
@@ -18,7 +24,35 @@ class Conference(AuditModel):
 
     def __unicode__(self):
         return self.name
+    
+    def save(self, **kwargs):
+        ''' Save function overrided inorder to send an email to all 
+            moderators when ever the conference data is changed '''
+        if self.id is None:
+            super(Conference, self).save(**kwargs)
+        else:
+            html_page = render_to_string("emails/conference_data_change_email.html",
+                                        {
+                                         'name': self.name,
+                                         'description':self.description,
+                                         'start_date':self.start_date,
+                                         'end_date':self.end_date,
+                                         'status':dict(CONFERENCE_STATUS_LIST)[self.status],
+                                         'modified_user':self.modified_by.get_full_name(),
+                                         'modified_date':self.modified_at.strftime("%d %b %Y"),
+                                         'class':'Conference',
+                                        }
+                                        )
+            subject = "Notification for data modification of the conference - "+self.name 
+            to_list = _get_conference_moderator_emails(self)
 
+            if to_list:
+                email_engine = EmailEngine()
+                email_engine.send_email(to_list,html_page,subject)
+            
+            super(Conference, self).save(**kwargs)
+        
+    
 
 class ConferenceModerator(AuditModel):
     """ List of Conference Moderators/Administrators  """
@@ -28,7 +62,27 @@ class ConferenceModerator(AuditModel):
 
     def __unicode__(self):
         return "{}[{}]".format(self.moderator.get_full_name(), self.conference)
-
+    
+    def save(self, **kwargs):
+        ''' Save function overrided inorder to send an email to all 
+            moderators when ever the conference data is changed '''
+        super(ConferenceModerator, self).save(**kwargs)
+        
+        html_page = render_to_string("emails/conference_data_change_email.html",
+                                    {
+                                     'conference_name':self.conference.name,
+                                     'moderator': self.moderator.get_full_name(),
+                                     'added_date':self.created_at.strftime("%d %b %Y"),
+                                     'class':'ConferenceModerator',
+                                    }
+                                    )
+        subject = "New Moderator added to the conference - "+self.conference.name 
+        to_list = _get_conference_moderator_emails(self.conference)
+        
+        if to_list:
+            email_engine = EmailEngine()
+            email_engine.send_email(to_list,html_page,subject)
+            
 
 class ConferenceProposalReviewer(AuditModel):
     """ List of global proposal reviewers """
@@ -42,3 +96,22 @@ class ConferenceProposalReviewer(AuditModel):
     class Meta:
         unique_together = ("conference", "reviewer")
 
+    def save(self, **kwargs):
+        ''' Save function overrided inorder to send an email to all 
+            moderators when ever the conference data is changed '''
+        super(ConferenceProposalReviewer, self).save(**kwargs)
+        
+        html_page = render_to_string("emails/conference_data_change_email.html",
+                                    {
+                                     'conference_name':self.conference.name,
+                                     'reviewer': self.reviewer.get_full_name(),
+                                     'added_date':self.created_at.strftime("%d %b %Y"),
+                                     'class':'ConferenceProposalReviewer',
+                                    }
+                                    )
+        subject = "New Proposal Reviewer added to the conference - "+self.conference.name 
+        to_list = _get_conference_moderator_emails(self.conference)
+        if to_list:
+            email_engine = EmailEngine()
+            email_engine.send_email(to_list,html_page,subject)
+    
