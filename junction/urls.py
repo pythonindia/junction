d72ec25e93ac8ce66dffd7729af077936e653eee
sivaa <siva@sivaa.in>
2015-01-04 19:23:23 +0530

    Proposals as a conference home now - redirection

--- junction/urls.py
+++ junction/urls.py
@@ -7,25 +7,27 @@ reference to them here.
 '''
 from __future__ import unicode_literals
 
-# Third Party Stuff
 from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 
 urlpatterns = patterns(
     '',
 
+    # Django Admin
     url(r'^nimda/', include(admin.site.urls)),
 
+    # Third Party Stuff
+    url(r'^accounts/', include('allauth.urls')),
+    url('^markdown/', include('django_markdown.urls')),
+
+    # Proposals related
     url(r'^(?P<conference_slug>[\w-]+)/proposals/', include('proposals.proposal_urls')),
     url(r'^(?P<conference_slug>[\w-]+)/proposal-comments/', include('proposals.comment_urls')),
     url(r'^(?P<conference_slug>[\w-]+)/proposal-votes/', include('proposals.vote_urls')),
 
-    url(r'^accounts/', include('allauth.urls')),
-    url('^markdown/', include('django_markdown.urls')),
-
-    # Static Pages
+    # Static Pages. TODO: to be refactored
     url(r'^speakers/$', TemplateView.as_view(template_name='static-content/speakers.html',), name='speakers-static'),
     url(r'^schedule/$', TemplateView.as_view(template_name='static-content/schedule.html',), name='schedule-static'),
     url(r'^venue/$', TemplateView.as_view(template_name='static-content/venue.html',), name='venue-static'),
@@ -33,6 +35,9 @@ urlpatterns = patterns(
     url(r'^blog/$', TemplateView.as_view(template_name='static-content/blog-archive.html',), name='blog-archive'),
     url(r'^coc/$', TemplateView.as_view(template_name='static-content/coc.html',), name='coc-static'),
     url(r'^faq/$', TemplateView.as_view(template_name='static-content/faq.html',), name='faq-static'),
+
+    # Proposals as conference home page. TODO: Needs to be enhanced
+    url(r'^(?P<conference_slug>[\w-]+)/', RedirectView.as_view(pattern_name="proposals-list")),
 
     # add at the last for minor performance gain
     url(r'^', include('pages.urls', namespace='pages')),
