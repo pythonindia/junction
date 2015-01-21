fdf1e62d73642d5e5894c81eb6a8b1e3c772c40d
Mudassir <mudassir.dev@gmail.com>
2015-01-22 00:44:15 +0530

    Include Grunt & Bower

---
+++ junction/static/Gruntfile.js
@@ -0,0 +1,29 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+     watch: {
+      less: {
+        files: ['./less/**/*.less'],
+        tasks: ['less']
+      }
+    },
+    less: {
+      compile: {
+        files: {
+          './css/app.css': './less/app.less'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['watch']);
+
+};
