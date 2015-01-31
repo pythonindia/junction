2653286efad4a950949a2ad8ab3cb11a1b57b6dd
Kracekumar Ramaraju <me@kracekumar.com>
2015-01-31 19:34:03 +0530

    Add npm, bower install, grunt compilation in regenerate

--- regenerate.sh
+++ regenerate.sh
@@ -8,3 +8,18 @@ python manage.py migrate --noinput
 
 echo "-> Generate sample data:"
 python manage.py sample_data
+
+echo "-> Some frontend changes"
+
+echo "-> Node install:"
+cd junction/static/
+npm install
+
+echo "-> Bower install:"
+bower install
+
+echo "-> Grunt:"
+grunt less
+
+cd -
+echo "You can keep running grunt for compiling less files, by running grunt inside static directory"
