892793d5ab81d8c3d110058f37bc538c35fa972f
Anuvrat Parashar <bhanuvrat@gmail.com>
2015-02-06 19:11:32 +0530

    flake8 warnings fixed

--- junction/__init__.py
+++ junction/__init__.py
@@ -2,4 +2,4 @@
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery_app import app as celery_app
+from .celery_app import app as celery_app #noqa
