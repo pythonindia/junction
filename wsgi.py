dd121be3fa36d88a43c4fbc7234b7077858bebd2
chillaranand <anand21nanda@gmail.com>
2015-02-07 13:00:29 +0530

    fix flake8 issue

--- wsgi.py
+++ wsgi.py
@@ -6,9 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
 application = get_wsgi_application()
