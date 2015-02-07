aab973c0f26a5ef175b41eda7cb11f18aa895050
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-02-07 19:34:36 +0530

    chore(): fix flake8 warning

--- wsgi.py
+++ wsgi.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
