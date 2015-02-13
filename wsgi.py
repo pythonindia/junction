5a49278de28a6ba765d1fd714b077ca785ea35fd
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-02-14 01:22:50 +0530

    update requiremnts to latest

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-from django.core.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
