c24278cd273915400ee9d2ca6647dea0e1e169f4
chillaranand <anand21nanda@gmail.com>
2015-02-17 00:03:09 +0530

    removed unused import

--- junction/conferences/models.py
+++ junction/conferences/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.db.models import signals
 from django.utils.translation import ugettext as _
 from django_extensions.db.fields import AutoSlugField
 from slugify import slugify
