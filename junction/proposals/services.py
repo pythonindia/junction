44df89e557d5c77acb82641ab487d985d0895088
Kracekumar Ramaraju <me@kracekumar.com>
2015-03-07 02:31:23 +0530

    Change posting tweet
    - Use oAuth2 for making twitter api call
    - Display only public proposals
    - Get exception msg and log it
    - Move TwitterApi at the end
    - Better unicode handling while tweeting

--- junction/proposals/services.py
+++ junction/proposals/services.py
@@ -75,18 +75,21 @@ def post_tweet_for_new_proposal(proposal_name, proposal_url):
     Post a tweet from junction twitter handler when a proposal is created.
     """
     try:
-        twitter_api = TwitterAPI(settings.TWITTER_CONSUMER_KEY,
-                                 settings.TWITTER_CONSUMER_SECRET,
-                                 settings.TWITTER_ACCESS_TOKEN_KEY,
-                                 settings.TWITTER_ACCESS_TOKEN_SECRET)
-        tweet_text = "New proposal: {0}".format(proposal_name)
+        tweet_text = u"New proposal: {0}".format(proposal_name)
 
         # twitter uses 26 chars for links, so text is limited to 114 chars.
         if len(tweet_text) > 114:
-            tweet_text = "{0}...".format(tweet_text[:110])
-        tweet = "{0} {1}".format(tweet_text, proposal_url)
+            tweet_text = u"{0}...".format(tweet_text[:110])
+        tweet = u"{0} {1}".format(tweet_text, proposal_url)
+
+        twitter_api = TwitterAPI(settings.TWITTER_CONSUMER_KEY,
+                                 settings.TWITTER_CONSUMER_SECRET,
+                                 settings.TWITTER_ACCESS_TOKEN_KEY,
+                                 settings.TWITTER_ACCESS_TOKEN_SECRET,
+                                 auth_type='oAuth2')
         response = twitter_api.request('statuses/update', {'status': tweet})
         return response
-    except:
-        logger.error('Unable to post tweet for {0} {1}'.
-                     format(proposal_name, proposal_url))
+    except Exception as e:
+        error_msg = unicode(e) + u'\nUnable to post tweet for {0} {1}'.format(
+            proposal_name, proposal_url)
+        logger.error(error_msg)
