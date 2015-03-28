c60f755987bd9c78a0db76fdb030a20934ac23df
Sai Krishna <saivnm5@gmail.com>
2015-03-28 14:03:06 +0530

    Issue 137 | Minor change in querying value of vote.

--- junction/proposals/templatetags/proposal_tags.py
+++ junction/proposals/templatetags/proposal_tags.py
@@ -9,7 +9,7 @@ def get_comment_vote_value(comment_id, user):
     comment_note_value = 0
     vote = models.ProposalCommentVote.objects.filter(proposal_comment_id=comment_id, voter=user)
     if vote.exists():
-        if vote.filter(up_vote=True).exists():
+        if vote[0].up_vote == True:
             comment_note_value = 1
         else:
             comment_note_value = -1
