831a476db53f3162142ffd22d866ffeda00244af
Anuvrat Parashar <bhanuvrat@gmail.com>
2015-04-08 16:19:43 +0530

    removing flake8 errors

--- tests/integrations/test_proposal_views.py
+++ tests/integrations/test_proposal_views.py
@@ -4,9 +4,10 @@ from .. import factories as f
 
 pytestmark = pytest.mark.django_db
 
+
 def test_list_proposals(client, settings):
     conference = f.create_conference()
-    proposal = f.create_proposal()
+    # proposal = f.create_proposal()
 
     url = reverse('proposals-list', kwargs={'conference_slug': conference.slug})
     r = client.get(url)
