12065a64c6586a6a0d2d4bc52eebab629653053a
htrap <htrapdev@gmail.com>
2015-07-14 03:59:02 +0530

    Add button for the content_urls of the proposal

--- junction/proposals/templatetags/proposal_filters.py
+++ junction/proposals/templatetags/proposal_filters.py
@@ -11,3 +11,10 @@ register = template.Library()
 @register.filter(name='reviewer_comments')
 def reviewer_comments(proposal, user):
     return proposal.get_reviewer_comments_count(user) > 0
+
+@register.filter(name='has_content_urls')
+def has_content_urls(proposal):
+    if proposal.content_urls:
+        return True
+    else:
+        return False
