45fe4df7edf4d9caf667741a34f32da7497d9396
Vignesh Sarma K <vignesh.sarma@gmail.com>
2015-08-06 23:19:36 +0530

    add migrations for simple history

---
+++ junction/conferences/migrations/0012_historicalconferenceproposalreviewer.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('conferences', '0011_auto_20150729_0131'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='HistoricalConferenceProposalReviewer',
+            fields=[
+                ('id', models.IntegerField(verbose_name='ID', db_index=True, auto_created=True, blank=True)),
+                ('created_at', models.DateTimeField(verbose_name='Created At', editable=False, blank=True)),
+                ('modified_at', models.DateTimeField(verbose_name='Last Modified At', editable=False, blank=True)),
+                ('active', models.BooleanField(default=True, verbose_name='Is Active?')),
+                ('nick', models.CharField(default='Reviewer', max_length=255, verbose_name='Nick Name')),
+                ('history_id', models.AutoField(serialize=False, primary_key=True)),
+                ('history_date', models.DateTimeField()),
+                ('history_type', models.CharField(max_length=1, choices=[('+', 'Created'), ('~', 'Changed'), ('-', 'Deleted')])),
+                ('conference', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.DO_NOTHING, db_constraint=False, blank=True, to='conferences.Conference', null=True)),
+                ('created_by', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.DO_NOTHING, db_constraint=False, blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('history_user', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True)),
+                ('modified_by', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.DO_NOTHING, db_constraint=False, blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('reviewer', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.DO_NOTHING, db_constraint=False, blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'ordering': ('-history_date', '-history_id'),
+                'get_latest_by': 'history_date',
+                'verbose_name': 'historical proposals reviewer',
+            },
+            bases=(models.Model,),
+        ),
+    ]
