931ae3ca9a77974983cb7f571f259673ba671e3b
Kracekumar Ramaraju <me@kracekumar.com>
2015-09-06 01:18:02 +0530

    Remove verify test code

--- tests/integrations/test_device_api.py
+++ tests/integrations/test_device_api.py
@@ -18,18 +18,18 @@ class TestDeviceListApiView(APITestCase):
         assert res.status_code == status.HTTP_201_CREATED
         assert res.data == data
 
-    def test_verify_device(self):
-        code = settings.DEVICE_VERIFICATION_CODE
+    # def test_verify_device(self):
+    #     code = settings.DEVICE_VERIFICATION_CODE
 
-        _uuid = uuid.uuid1()
-        data = {'uuid': str(_uuid)}
-        res = self.client.post('/api/v1/devices/', data, format='json')
+    #     _uuid = uuid.uuid1()
+    #     data = {'uuid': str(_uuid)}
+    #     res = self.client.post('/api/v1/devices/', data, format='json')
 
-        assert res.status_code == status.HTTP_201_CREATED
+    #     assert res.status_code == status.HTTP_201_CREATED
 
-        url = '/api/v1/devices/{}/'.format(_uuid)
+    #     url = '/api/v1/devices/{}/'.format(_uuid)
 
-        resp = self.client.post(url, {'code': code},
-                                format='json')
+    #     resp = self.client.post(url, {'code': code},
+    #                             format='json')
 
-        assert resp.status_code == status.HTTP_200_OK
+    #     assert resp.status_code == status.HTTP_200_OK
