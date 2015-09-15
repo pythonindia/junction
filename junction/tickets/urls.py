1c639d1e866aabf248fd3d3465156c6caa34232b
ChillarAnand <anand21nanda@gmail.com>
2015-09-16 00:24:32 +0530

    Add admin view to call sync_data command

---
+++ junction/tickets/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+# Third Party Stuff
+from django.conf.urls import include, patterns, url
+
+#Junction Stuff
+from . import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^sync_data/$', views.sync_data, name='sync_data'),
+)
+
---
+++ junction/tickets/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+# Third Party Stuff
+from django.core.management import call_command
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+#Junction Stuff
+from .forms import SyncDataForm
+
+
+@require_http_methods(['GET', 'POST'])
+def sync_data(request):
+    """
+    View to call sync_data management command.
+    """
+    if not request.user.is_superuser:
+        raise PermissionDenied
+
+    form = SyncDataForm()
+
+    if request.method == 'GET':
+        return render(request, 'sync_data.html', {'form': form})
+
+    call_command('sync_data')
+    return render(request, 'sync_data.html',
+                  {'form': form, 'message': 'Data synced'})
