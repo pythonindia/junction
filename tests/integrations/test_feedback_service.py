238b92a561f9285f7030b1dae51a3ffeb3de579e
Kracekumar Ramaraju <me@kracekumar.com>
2016-01-05 22:43:51 +0530

    Use set to compare list of values

--- tests/integrations/test_feedback_service.py
+++ tests/integrations/test_feedback_service.py
@@ -15,7 +15,7 @@ def test_get_feedback_questions_without_conference():
 
 
 def test_get_feedback_questions_with_conference():
-    schedule_item_types = ['Workshop', 'Talk']
+    schedule_item_types = set(['Workshop', 'Talk'])
     num_choice_questions = 2
     num_text_questions = 1
     objects = factories.create_feedback_questions(
@@ -26,7 +26,7 @@ def test_get_feedback_questions_with_conference():
     conference = objects['conference']
     result = service.get_feedback_questions(conference_id=conference.id)
 
-    assert list(result.keys()) == schedule_item_types
+    assert set(result.keys()) == schedule_item_types
     for item_type in schedule_item_types:
         assert len(result[item_type]['text']) == num_text_questions
         assert len(result[item_type]['choice']) == num_choice_questions
