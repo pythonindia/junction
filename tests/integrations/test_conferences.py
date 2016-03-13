27e138f62e93d1e51ae5e41c26854795ff87b7f0
Kracekumar Ramaraju <me@kracekumar.com>
2016-03-13 13:50:11 +0530

    Assert on str objects rather than byte in Py3k

--- tests/integrations/test_conferences.py
+++ tests/integrations/test_conferences.py
@@ -4,7 +4,7 @@ from .. import factories as f
 def test_conferences(client, db):
     conference = f.ConferenceFactory()
     response = client.get('/')
-    assert conference.name in response.content
+    assert str(conference.name) in str(response.content)
 
     # should not display the conference if it's set as deleted
     conference.deleted = True
