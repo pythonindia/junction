834b70eeb0f0ab09a7cf36848e0db89b46770635
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-07-02 11:58:16 +0530

    Add bumpversion

--- junction/__init__.py
+++ junction/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+__version__ = '0.3.1'
+__version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
