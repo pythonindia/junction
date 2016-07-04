5ecccfed3fb30219f05a2917a233875dd3f1d399
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-07-05 02:47:10 +0530

    Added Models for Users Profile

--- junction/profiles/admin.py
+++ junction/profiles/admin.py
@@ -1,3 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Profiles
+
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('__unicode__','city', 'contact_no')
+
+admin.site.register(Profiles, UserAdmin)
--- junction/profiles/models.py
+++ junction/profiles/models.py
@@ -1,3 +1,17 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profiles(models.Model):
+	''' 
+	It stores the City/Phone Details of the User.
+	'''
+	user=models.OneToOneField(User)
+	city=models.CharField(max_length=100, blank=False, null=False)
+	contact_no = models.CharField(max_length=10, blank=False, null=False)
+
+	created = models.DateTimeField("Created", null=True, auto_now_add=True)
+	modified = models.DateTimeField("Last Modified", null=True, auto_now=True)
+
+	def __unicode__(self):
+		return self.user.username
