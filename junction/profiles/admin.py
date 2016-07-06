e20375e53248b6ded0e51c311c9c7aabfb34f5e1
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-07-06 18:39:58 +0530

    Removed Errors & tests written

--- junction/profiles/admin.py
+++ junction/profiles/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profiles
+from .models import Profile
 
 
 class UserAdmin(admin.ModelAdmin):
 	list_display = ('__unicode__','city', 'contact_no')
 
-admin.site.register(Profiles, UserAdmin)
+admin.site.register(Profile, UserAdmin)
