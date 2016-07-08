1ae6e0a5de5dbd420d5b7cb61a43fddc67aea6bc
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-07-09 02:03:00 +0530

    Tests written and Models created

--- junction/profiles/models.py
+++ junction/profiles/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-	'''
+    '''
     It stores the City/Phone Details of the User.
-	'''
-	user = models.OneToOneField(User)
-	city = models.CharField(max_length=100, blank=False, null=False)
-	contact_no = models.CharField(max_length=10, blank=False, null=False)
+    '''
+    user = models.OneToOneField(User)
+    city = models.CharField(max_length=100, blank=False, null=False)
+    contact_no = models.CharField(max_length=10, blank=False, null=False)
 
-	def __unicode__(self):
-		return self.user.username
+    def __unicode__(self):
+        return self.user.username
