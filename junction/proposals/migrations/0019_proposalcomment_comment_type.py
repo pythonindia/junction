4eb3cad1472a83eb4c25c4b7e6553b7466b0307b
Chillar Anand <anand21nanda@gmail.com>
2016-08-06 20:24:01 +0530

    Remove unique together constraint

---
+++ junction/proposals/migrations/0019_proposalcomment_comment_type.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('proposals', '0018_auto_20160806_1727'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='proposalcomment',
+            name='comment_type',
+            field=models.PositiveSmallIntegerField(default=0, choices=[(0, 'All general comments'), (1, 'Second phase voting')]),
+            preserve_default=True,
+        ),
+    ]
--- junction/proposals/models.py
+++ junction/proposals/models.py
@@ -262,7 +262,6 @@ class ProposalSectionReviewerVote(TimeAuditModel):
         return "[{}] {}".format(self.vote_value, self.proposal)
 
     class Meta:
-        unique_together = ("proposal", "voter")
         verbose_name = 'ProposalSectionReviewerVote'
 
 
