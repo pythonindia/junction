72e8f75acc92f8794d66b132c00ed40d7172c8a4
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-09-10 00:25:14 +0530

    flake8 fixes

--- junction/profiles/forms.py
+++ junction/profiles/forms.py
@@ -5,5 +5,5 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['city', 'contact_no',]
-        exclude = ['user',]
+        fields = ['city', 'contact_no', ]
+        exclude = ['user', ]
