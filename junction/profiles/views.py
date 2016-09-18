caafce36aabe2d6feee66f69559de986ef8007f3
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-09-18 14:34:01 +0530

    flake8 fixes

--- junction/profiles/views.py
+++ junction/profiles/views.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 
 # Third Party Stuff
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
