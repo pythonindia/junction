c07543daef680328040c24853a655a297aad7edd
chillaranand <anand21nanda@gmail.com>
2016-10-02 23:00:41 +0530

    Configure pagination globally

--- junction/proposals/api.py
+++ junction/proposals/api.py
@@ -1,20 +1,13 @@
 from rest_framework import generics
-from rest_framework.pagination import PageNumberPagination
 
 from junction.base.constants import ProposalStatus
 from junction.proposals import serializers
 from junction.proposals.models import Proposal
 
 
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 2
-    page_size_query_param = 'page'
-
-
 class ProposalListApiView(generics.ListAPIView):
 
     serializer_class = serializers.ProposalListSerializer
-    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         queryset = Proposal.objects.filter(deleted=False, status=ProposalStatus.PUBLIC)
