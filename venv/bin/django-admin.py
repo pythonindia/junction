1814ae5048239bf29af1b8a9c75a68ac5dfa5ff8
parthvivala <parthvi.vala@gmail.com>
2017-11-07 15:03:51 +0530

    add custom django management command to export all proposers information

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/parthvivala/code/OpenSourceContribution/junction/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
