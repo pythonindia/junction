6d924ba68420415d325e75c13e5193a5b5df1d4b
Pradyun Gedam <pradyunsg@gmail.com>
2020-03-11 01:19:55 +0530

    Declare the app name for namespaced-include

--- junction/profiles/urls.py
+++ junction/profiles/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "junction.profiles"
+
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^edit/$', views.profile, name='profile'),
-
-
 ]
