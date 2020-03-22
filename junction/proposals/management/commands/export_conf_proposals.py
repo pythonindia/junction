3822dd347bd40a5eb9ed95347a0f604f18819a0e
Mayank Prasoon <mayank.prasoon@outlook.com>
2020-03-22 23:48:47 +0530

    Change export to csv

--- junction/proposals/management/commands/export_conf_proposals.py
+++ junction/proposals/management/commands/export_conf_proposals.py
@@ -1,7 +1,7 @@
 
-import io
 from os.path import join
 from settings.common import ROOT_DIR
+import pandas as pd
 
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
@@ -44,37 +44,22 @@ class Command(BaseCommand):
         vote_values_list = [v.vote_value for v in vote_values_list]
         vote_values_desc = tuple(i.description for i in ProposalSectionReviewerVoteValue.objects.order_by('-vote_value'))
         header = ('Proposal Type', 'Title', 'Sum of reviewer votes', 'No. of reviewer votes') + tuple(vote_values_desc) + ('Public votes count', 'Vote comments')
-        output = io.BytesIO()
+        df = pd.DataFrame(columns=header)
+        i = 0
 
-        with Workbook(output) as book:
-            for section in proposal_sections:
-                sheet = book.add_worksheet(section.name[:30])
-                cell_format = book.add_format({'bold': True})
-                sheet.write_row(0, 0, header, cell_format)
+        for section in proposal_sections:
+            section_proposals = [p for p in proposal_qs if p.proposal_section == section]
 
-                section_proposals = [p for p in proposal_qs if p.proposal_section == section]
+            for index, p in enumerate(section_proposals, 1):
+                vote_details = tuple(p.get_reviewer_votes_count_by_value(v) for v in vote_values_list)
+                vote_comment = '\n'.join([comment.comment for comment in p.proposalcomment_set.filter(vote=True, deleted=False)])
+                row = (p.proposal_type.name, p.title, p.get_reviewer_votes_sum(), p.get_reviewer_votes_count(),) + vote_details + (p.get_voutes_count(), vote_comment,)
+                df.loc[i] = row
+                i+=1
 
-                for index, p in enumerate(section_proposals, 1):
-                    vote_details = tuple(p.get_reviewer_votes_count_by_value(v) for v in vote_values_list)
-                    vote_comment = '\n'.join([comment.comment for comment in p.proposalcomment_set.filter(vote=True, deleted=False)])
-                    row = (p.proposal_type.name, p.title, p.get_reviewer_votes_sum(), p.get_reviewer_votes_count(),) + vote_details + (p.get_voutes_count(), vote_comment,)
-                    if p.get_reviewer_votes_count_by_value(ProposalSectionReviewerVoteValue.objects.get(vote_value=ProposalReviewVote.NOT_ALLOWED).vote_value) > 0:
-                        cell_format = book.add_format({'bg_color': 'red'})
-                    elif p.get_reviewer_votes_count_by_value(ProposalSectionReviewerVoteValue.objects.get(vote_value=ProposalReviewVote.MUST_HAVE).vote_value) > 2:
-                        cell_format = book.add_format({'bg_color': 'red'})
-                    elif p.get_reviewer_votes_count() < 2:
-                        cell_format = book.add_format({'bg_color': 'yellow'})
-                    else:
-                        cell_format = None
+        csv_file_name = "%s-%s" % (user.name, conference.name)
+        csv_file_location = join(ROOT_DIR, 'csvs', excel_file_name)
 
-                    sheet.write_row(index, 0, row, cell_format)
-
-        output.seek(0)
-
-        excel_file_name = "%s-%s" % (user.name, conference.name)
-        excel_file_location = join(ROOT_DIR, 'excels', excel_file_name)
-
-        with open(excel_file_location, 'wb') as excelfile:
-            excelfile.write(output.read())
+        df.to_csv(csv_file_location, sep='\t')
 
         self.stdout.write("Successfully created the excel file")
