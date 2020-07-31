82d63874b12ac8e8734bef1bf1e1fa51f468466e
Abhishek Mishra <geekabhi@outlook.com>
2020-08-01 01:58:07 +0530

    Fix linting error

--- junction/proposals/migrations/0029_auto_20200707_0844.py
+++ junction/proposals/migrations/0029_auto_20200707_0844.py
@@ -8,18 +8,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('proposals', '0028_auto_20200617_2337'),
+        ("proposals", "0028_auto_20200617_2337"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='historicalproposal',
-            name='is_first_time_speaker',
+            model_name="historicalproposal",
+            name="is_first_time_speaker",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='proposal',
-            name='is_first_time_speaker',
+            model_name="proposal",
+            name="is_first_time_speaker",
             field=models.BooleanField(default=False),
         ),
     ]
