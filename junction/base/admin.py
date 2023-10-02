f118d094be76afc617f9bd5e65cf300eae18cf7d
soumendrak <soumendra.s@outlook.com>
2023-10-02 14:07:47 +0530

    feat("junction/base/admin.py"): Add Export CSV to all the lists in Junction

--- junction/base/admin.py
+++ junction/base/admin.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 
 def save_model(self, request, obj, form, change):
@@ -20,6 +23,23 @@ class TimeAuditAdmin(admin.ModelAdmin):
         "created_at",
         "modified_at",
     )
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected as CSV"
 
 
 class AuditAdmin(TimeAuditAdmin):
