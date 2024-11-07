d5087672a3f0763a93606f601b37b5d4256cd48b
Priyanshi Soni <sonipriyanshi.ps99@gmail.com>
2024-11-07 16:58:52 +0530

    Update urls.py
    added the simple way to attaching url in urls.py file .

--- junction/profiles/urls.py
+++ junction/profiles/urls.py
@@ -6,5 +6,7 @@ app_name = "junction.profiles"
 
 urlpatterns = [
     re_path(r"^$", views.dashboard, name="dashboard"),
+    #you can also use instead of re_path , path and include like 
+    #path(url_type, view_function, name)
     re_path(r"^edit/$", views.profile, name="profile"),
 ]
